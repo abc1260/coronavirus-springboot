d2a24b4a8c57e7699f62c1826ed1a465ee421680
Philip Chauvet <pchauvet@gmail.com>
2020-03-09 18:33:36 -0400

    06_setup_application_base_test

---
+++ src/test/java/com/aikiinc/coronavirus/CornaVirusControllerTest.java
@@ -0,0 +1,30 @@
+package com.aikiinc.coronavirus;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CornaVirusControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("{\"message\":\"Greetings from Spring Boot!\"}")));
+	}
+
+	
+}
