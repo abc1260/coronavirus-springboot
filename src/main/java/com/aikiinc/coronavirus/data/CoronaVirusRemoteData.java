f34c66d0d7aa6bd7315fa72b6754a2d0b8d83eaf
Philip Chauvet <pchauvet@gmail.com>
2020-03-10 04:40:58 -0400

    12_implement_remotedata_access - 06_read_remote_data_code

--- src/main/java/com/aikiinc/coronavirus/data/CoronaVirusRemoteData.java
+++ src/main/java/com/aikiinc/coronavirus/data/CoronaVirusRemoteData.java
@@ -1,15 +1,20 @@
 package com.aikiinc.coronavirus.data;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.Optional;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 public class CoronaVirusRemoteData {
 	private Logger log = LoggerFactory.getLogger(CoronaVirusRemoteData.class);
-	public final static String SOURCE_URL_PREFIX = "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports/";
+	public final static String SOURCE_URL_PREFIX = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/";
 	private String sourceUrlPrefix;
 	private URL siteUrl;
 	private String dateLoaded;
@@ -27,7 +32,8 @@ public class CoronaVirusRemoteData {
 	}
 
 	void setRemoteConnection() throws CoronaVirusDataException {
-		//String sdate = LocalDate.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+		// String sdate =
+		// LocalDate.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
 		String sdate = LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
 		log.debug("Load coronavirus data for date: " + sdate);
 
@@ -36,7 +42,7 @@ public class CoronaVirusRemoteData {
 			this.siteUrl = new URL(sourceUrl);
 
 			siteUrl.getContent();
-			
+
 			this.dateLoaded = sdate;
 
 			log.debug("\tLoading data from: " + siteUrl);
@@ -44,6 +50,31 @@ public class CoronaVirusRemoteData {
 			log.warn("\tCould not load data from: " + sourceUrl);
 			log.warn("\tException: " + e.getMessage());
 		}
+	}
+
+	Optional<Iterable<CSVRecord>> readRemoteData() throws CoronaVirusDataException {
+		log.info("Read remote data from:  " + siteUrl);
+		Optional<Iterable<CSVRecord>> records = Optional.empty();
+
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(siteUrl.openStream()));
+
+			// Get the iterable records
+			Iterable<CSVRecord> trecords = CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreEmptyLines().parse(br);
+			records = Optional.of(trecords);
+		} catch (IOException e) {
+			log.warn("Remote data was NOT read from: " + siteUrl);
+			throw new CoronaVirusDataException(e);
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException e) {
+			}
+		}
+
+		return records;
 	}
 
 }
--- src/test/java/com/aikiinc/coronavirus/data/CoronaVirusRemoteDataTest.java
+++ src/test/java/com/aikiinc/coronavirus/data/CoronaVirusRemoteDataTest.java
@@ -1,5 +1,8 @@
 package com.aikiinc.coronavirus.data;
 
+import java.util.Optional;
+
+import org.apache.commons.csv.CSVRecord;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -26,6 +29,26 @@ public class CoronaVirusRemoteDataTest {
 			coronaVirusRemoteData.process();
 		} catch (Exception e) {
 			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void readRemoteData() {
+		try {
+			coronaVirusRemoteData.process();
+			
+			Optional<Iterable<CSVRecord>> records = coronaVirusRemoteData.readRemoteData();
+			if (records.isPresent()) {
+				Assert.assertTrue(records.get().iterator().hasNext());
+				
+				try {
+					records.get().forEach(e -> System.out.println(e));
+				} catch (Exception e) {
+				}
+			} else
+				Assert.fail("No CSVRecord was read");
+		} catch (CoronaVirusDataException e) {
 			Assert.fail(e.getMessage());
 		}
 	}
